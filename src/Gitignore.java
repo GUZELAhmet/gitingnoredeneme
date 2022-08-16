a033c83ce9f2e78d94da6904b9924772e22802e9
Ahmet Guzel <gzlhmt@gmail.com>
2022-08-16 23:02:20 +0300

    d10

--- src/Gitignore.java
+++ src/Gitignore.java
@@ -2,5 +2,6 @@ public class Gitignore {
     public static void main(String[] args) {
         System.out.println("gitignore sdeneme");
         System.out.println("gitignore sdeneme  branch");
+        System.out.println("gitignore sdeneme abc branch");
     }
 }
